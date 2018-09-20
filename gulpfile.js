f21b3ad27f67ac16ba48e257740fc8aef09a28be
Aba Gardon <aba.gardon@bk.ru>
2018-09-21 02:09:48 +0400

    Подключает задачи для gulp

---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var posthtml = require("gulp-posthtml");
+var postcss = require("gulp-postcss");
+var include = require("posthtml-include");
+var plumber = require('gulp-plumber');
+var autoprefixer = require("autoprefixer");
+var del = require("del");
+var run = require("run-sequence");
+var imagemin = require("gulp-imagemin");
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+
+var path = {
+    build: {
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: {
+        html: '*.html',
+        js: 'js/*.js',
+        style: 'scss/main.scss',
+        img: 'img/**/*.{png,jpg,svg}',
+        fonts: 'fonts/**/*.{woff,woff2}'
+    },
+    clean: './build'
+};
+
+gulp.task("html", function () {
+    return gulp.src(path.src.html)
+      .pipe(posthtml([
+        include()
+      ]))
+      .pipe(gulp.dest(path.build.html))
+      .pipe(server.stream());
+});
+
+gulp.task("style", function() {
+    gulp.src(path.src.style)
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest(path.build.css))
+        .pipe(minify())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest(path.build.css))
+        .pipe(server.stream());
+});
+
+gulp.task('compress', function (done) {
+    del(path.build.js);
+    pump([
+        gulp.src('js/*.js'),
+        uglify(),
+        gulp.dest(path.build.js)
+      ],
+      done
+    );
+});
+
+gulp.task("copy", function () {
+    return gulp.src([
+      path.src.fonts,
+      path.src.img,
+      path.src.js
+    ], {
+      base: "."
+    })
+    .pipe(gulp.dest(path.build.html));
+});
+
+gulp.task("watch", function() {
+    server.init({
+      server: path.build.html
+    });
+    gulp.watch("scss/**/**/*.scss", ["style"]);
+    gulp.watch("*.html", ["html"]);
+});
+
+gulp.task("clean", function () {
+    return del(path.clean);
+});
+
+gulp.task("images", function () {
+    return gulp.src(path.src.img)
+        .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.svgo()
+        ]))
+    .pipe(gulp.dest(path.build.img));
+});
+
+gulp.task("build", function (done) {
+    run(
+        "clean",
+        "copy",
+        "style",
+        "html",
+        done
+    );
+});
